7ac35e84ce0d99f57e27be20abac12341a782cae
keesaev <kostia.234@mail.ru>
2020-08-06 18:11:25 +0300

    Добавил мьютексы для синхронизации std консольного вывода

--- server.cpp
+++ server.cpp
@@ -1,6 +1,31 @@
 #include "server.h"
 
-server::server()
+Server::Server(boost::asio::io_service *io_service, boost::mutex *outputMutex, unsigned short port) :
+    io_service(io_service), outputMutex(outputMutex), port(port)
 {
-
+    acceptor = new ip::tcp::acceptor
+            (*io_service, ip::tcp::endpoint(ip::tcp::v4(), port));
 }
+
+void Server::listen(){
+
+    char buf[1024];
+    boost::system::error_code errorCode;
+
+    while( true ){
+        ip::tcp::socket socket(*io_service);
+        acceptor->accept(socket);
+        socket.read_some(boost::asio::buffer(buf), errorCode);
+
+        outputMutex->lock();
+        std::cout << "Server reads: ";
+        printf(buf);
+        std::cout << std::endl;
+        outputMutex->unlock();
+
+        socket.write_some(boost::asio::buffer(buf), errorCode);
+
+        socket.close();
+    }
+}
+
--- server.h
+++ server.h
@@ -1,12 +1,28 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <QObject>
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/thread/mutex.hpp>
 
-class server
+using namespace boost::asio;
+using namespace std;
+
+class Server
 {
 public:
-    server();
+    Server(io_service *io_service, boost::mutex *outputMutex, unsigned short port);
+    ~Server(){
+        delete acceptor;
+    }
+    void listen();
+private:
+    io_service *io_service;
+    boost::mutex *outputMutex;
+    ip::tcp::acceptor *acceptor;
+
+    unsigned short port;
+    std::string ip = "127.0.0.1";
 };
 
 #endif // SERVER_H
