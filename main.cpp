0efed5de532885de825087086769416468af73df
keesaev <kostia.234@mail.ru>
2020-08-07 17:43:00 +0300

    Some comments

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     boost::asio::io_service io_service_server;
     boost::mutex outputMutex; // Used to synchronise console output
 
-    unsigned short port = 8001;
+    unsigned short port = 8001; // Random port
 
     Server *serv = new Server(&io_service_server, &outputMutex, port);
     boost::thread serverThread(&Server::listen, serv);
@@ -27,8 +27,13 @@ int main(int argc, char *argv[])
                                      std::to_string(i + 1),
                                      port));
         threads.push_back(boost::thread{&Client::write, clients[i]});
-        threads[i].join();
+        //threads[i].join(); // Thread executing one by one
     }
+
+    // Threads executing in random order
+
+    for(int i = 0; i < 5; i++)
+        threads[i].join();
 
     serverThread.join();
 
